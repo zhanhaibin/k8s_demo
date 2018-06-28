797bb6574db2b8440c8701738fd26a36c50e1ffb
zhanhaibin <haibin.zhan@gmail.com>
2018-06-28 11:30:47 +0800

    update

---
+++ src/main/java/com/ava/k8s_demo/K8sRestfulClient2.java
@@ -0,0 +1,17 @@
+package com.ava.k8s_demo;
+
+import javax.ws.rs.core.Response;
+
+public interface K8sRestfulClient2 {
+	public String get(K8sParams params); 	//获取单个资源对象
+	public String list(K8sParams params); 	//获取资源对象列表
+	public String create(K8sParams params); //创建资源对象
+	public Response create2(K8sParams params); //创建资源对象
+	public String delete(K8sParams params); //删除某个资源对象
+	public String update(K8sParams params); //部分更新某个资源对象
+	public String updateWithMediaType(K8sParams params,String mediaType);	//通过mediaType，实现Merge
+	public String replace(K8sParams params);	//替换某个资源对象
+	public String options(K8sParams params);
+	public String head(K8sParams params); 
+	public void close() ;
+}
